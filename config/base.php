554b794d8f4bc14dc56fbb026cdb50aad4549d46
fuxjian01 <805392647@qq.com>
2018-12-25 16:15:33 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,102 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => '205015',
+  'onedrive_root' => '',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'dwhhVYUS42_@jzrRWY155_{',
+  'client_id' => '57aea403-8027-4a73-973a-0cce6d771312',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjT5IK5rzOCBVftigSdZIOs2Z7mojfaWBlgq0FFdj7G82A1AVaQw4XlIX4nvmS4rbHr9Dg4z9tvuSA_PSBNPV_yqmwz8w0y7bg4C81YEDL5LfYZl_AZB2p20nDCNj8x4ggAOk6SO2BV42M5VJJnxeYVOpPR7Op8Rhq4MzatbinT8WG8eo2fNBJZUDSjK1Ig3hCGYY-khU8RvpZnlRwU-a_0wCy6Eetl3vgLC5hCVCir812xZu6iKYs-CG-ZAvaEISFBZpO-6PN0p7NO1Z1v-Na1MV0Wcn2rx_OCwPgMVomFB2KwDT4PzLp-ZxPdicwAG76uExce2Os24S7n5cnkRwIy9q5vg-1ecZhY0KMIubDf9NQ5-5FsoJYcoqZMX_iz0eVfyf_hl_GAhhYrpnwfMTYOf6JrqEVAy1F3fZpng-QdsofTiAlqIIlcaBrOk-iHRJDC4wJlZlHG0crPdeb9wYyYuSu5sHoLFNUeKxsSQt0ButkleF87Qe6IWZoKGnkIDu4Bi8RDzR_4mJhyVuwr23MMwIg6B4oV8keb_GLAxHMwWGUlafE8xjlqZrxhZGM27WNR9e5-g013v1d4dELFcW7OjTgjUNzUy4sefPVJQ6PFauP4NS1yCcOUovFF4zBXrQLFxvqMshWNMDtUPf4-QS-_YeIupBwuInWfh7A_WV7u8IPrOvI9xNjwfl0-eeD9Sg_3wOE70T5nOjDhe8D_Q-CRrKhSJY_QOHKUwOKnalBRNPGlCvEqkN5yG0InXI8EPf7HIAA',
+);
